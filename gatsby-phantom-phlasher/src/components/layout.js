3e8da8a53938efc19d0539d0b58a97a28456258b
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-10-29 23:57:18 +0100

    adding photo gallery

--- gatsby-phantom-phlasher/src/components/layout.js
+++ gatsby-phantom-phlasher/src/components/layout.js
@@ -8,7 +8,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import $ from "jquery"
 import "../styles/main.css"
 
 const Layout = ({ children }) => {
